2134d8065f8fefe24a243ca6be5f93f8b8b0aa1a
Charly Mannion <charlottemannion100@gmail.com>
2018-04-23 12:56:49 +0100

    step 04 initialization and balance methods

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require './lib/oystercard.rb'
+
 describe Oystercard do
-  
+
+  describe '#balance' do
+    it 'a should have a balance of 0' do
+      expect(subject.balance).to eq 0      
+    end
+  end
 end
