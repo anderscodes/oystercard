6450a4385eec281542b8ec5f7f9e05b65ec8235a
anderscodes <anders.j.persson@hotmail.com>
2018-04-23 15:00:25 +0100

    step 6 tested and created Max balance implemented

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,23 @@
 class Oystercard
-attr_reader :balance
 
-  def initialize(balance = 0)
+  attr_reader :balance
+
+  MAX_BALANCE = 90
+  DEFAULT_BALANCE = 0
+
+  def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(amount)
+    raise "This card has reached its limit of #{MAX_BALANCE}" if limit_reached?
     @balance += amount
- end
+  end
+
+private
+
+  def limit_reached?
+    balance >= MAX_BALANCE
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,13 @@
 require './lib/oystercard.rb'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
 
-subject(:oystercard) { described_class.new }
+  describe '#initialize' do
+    it 'should have a default balance equal to DEFAULT_BALANCE' do
+    expect(oystercard.balance).to eq Oystercard::DEFAULT_BALANCE
+  end
+end
 
   describe '#balance' do
     it 'a should have a balance of 0' do
@@ -10,11 +15,15 @@ subject(:oystercard) { described_class.new }
     end
   end
 
-describe '#top_up' do
-  it 'should allow user to increase balance of the oystercard' do
-    oystercard.top_up(10)
-    expect(oystercard.balance).to eq 10
+  describe '#top_up' do
+    it 'should allow user to increase balance of the oystercard' do
+      oystercard.top_up(10)
+      expect(oystercard.balance).to eq 10
+    end
+    it 'should raise an error if the balance is at max limit' do
+      allow(oystercard).to receive(:limit_reached?) { true }
+      expect { oystercard.top_up(10) }.to raise_error "This card has reached its limit of #{Oystercard::MAX_BALANCE}"
+    end
   end
-end
 
 end
