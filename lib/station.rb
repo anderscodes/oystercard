f98d40e4b72e48697adc3dfe8cee971daf3365a3
George Sykes <georgesykes86@gmail.com>
2018-04-24 16:17:00 +0100

    Adds auto detect zone

--- lib/station.rb
+++ lib/station.rb
@@ -1,10 +1,21 @@
+require 'csv'
+
 class Station
 
 attr_reader :name, :zone
 
-  def initialize(name, zone)
+  def initialize(name, station_path = './resources/stations.csv')
     @name = name
-    @zone = zone
+    @station_path = station_path
+    @zone = get_zone(name)
+  end
+
+  def get_zone(name)
+    CSV.foreach(@station_path) do |line|
+      station, zone = line
+      return zone.to_i if station == name
+    end
+    raise "Station not recognized"
   end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,17 +1,28 @@
 require 'station.rb'
+require 'csv'
 
 describe Station do
-
-  subject(:station) { described_class.new(name, zone) }
-  let(:name) { 'London Bridge' }
-  let(:zone) { 1 }
+  before do
+    stations_filename = './resources/stations.csv'
+    @stations = CSV.read(stations_filename)
+  end
+  let(:name) { @stations[0][0] }
+  let(:zone) { @stations[0][1].to_i }
+  subject(:station) { described_class.new(name) }
+  let(:bad_name) { 'Swindon' }
 
   describe '#initialize' do
     it 'sets name to name' do
       expect(station.name).to eq name
     end
-    it 'sets zone to zone' do
+    it 'sets zone automatically' do
       expect(station.zone).to eq zone
+    end
+
+    context 'when the name is not valid' do
+      it 'raises an error' do
+        expect { Station.new(bad_name) }.to raise_error("Station not recognized")
+      end
     end
   end
 end
